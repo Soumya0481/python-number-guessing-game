5b2d283eab06fd9970b456760e2b92dad9a664dd
SOUMYA <nagure7019795023@gmail.com>
2025-11-07 22:26:45 +0530

    Enable game restart option after finishing
    Added functionality to repeat the game after completion.

--- guess_the_number.py
+++ guess_the_number.py
@@ -22,26 +22,46 @@ print("-----------------------------------------------------")
 target = random.randint(min_val, max_val)
 
 attempts = 10
-while attempts > 0:
-    userguess = input("Guess the target number or quit the game Q:\n ").strip()
-    if userguess.lower() == 'q':
+
+while True:
+    
+    while attempts > 0:
+        userguess = input("Guess the target number or quit the game Q:\n ").strip()
+        
+        if userguess.lower() == 'q':
+            print("Thanks for playing! Goodbye.")
+            break
+        
+        guess = int(userguess)
+        attempts -= 1
+        
+        if guess < target:
+            print("Your guess number is too small! Think bigger.\n")
+        
+        elif guess > target:
+            print("Your guess number is too high! Think smaller.\n")
+        
+        else:
+            print("Congratulations! You've guessed the number!\n\n")
+            break
+        
+        if attempts == 0:
+            print(f"Sorry, you've run out of attempts. The number was {target}\n\n")
+        else:
+            print(f"You have {attempts} attempts left.\n")
+
+    repeat = input("Do you want to continue the game? (y/n): ").strip().lower()
+    if repeat == 'n':
         print("Thanks for playing! Goodbye.")
         break
-    guess = int(userguess)
-    attempts -= 1
-    if guess < target:
-        print("Your guess number is too small! Think bigger.\n")
-    elif guess > target:
-        print("Your guess number is too high! Think smaller.\n")
     else:
-        print("Congratulations! You've guessed the number!\n\n")
-        break
-    if attempts == 0:
-        print(f"Sorry, you've run out of attempts. The number was {target}\n\n.")
-    else:
-        print(f"You have {attempts} attempts left.\n")
+        # Resetting the game
+        choice = input("Do you want to set your own range? (Y/N): ").strip().lower()
+        target = random.randint(min_val, max_val)
+        attempts = 10
 
 print("Game Over.\n")
+
 
 
 
